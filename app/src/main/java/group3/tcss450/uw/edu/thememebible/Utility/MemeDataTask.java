ecec360033cd02e8b37f4d3b300b90fb3c5b7a3b
bunny <phansieeec@gmail.com>
2017-05-24 14:55:04 -0700

    test

--- app/src/main/java/group3/tcss450/uw/edu/thememebible/Utility/MemeDataTask.java
+++ app/src/main/java/group3/tcss450/uw/edu/thememebible/Utility/MemeDataTask.java
@@ -105,6 +105,8 @@ public class MemeDataTask extends AsyncTask<String, Void, String> {
                         memeList.add(Meme.getMeme(jsonObject));
                     }
 
+                    //peter sucks
+                    //I know right.
                     // if we weren't able to populate memeList, notify user to try again
                     if (memeList.isEmpty()) {
                         Toast.makeText(mContext, "No memes returned. Try again?", Toast.LENGTH_LONG).show();
